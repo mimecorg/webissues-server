e877000f4afac0b76b008cb7e8ad801a495874f3
mimecorg <mimec@mimec.org>
2011-11-22 23:11:18 +0000

    checking latest version on the about page
    git-svn-id: svn+ssh://svn.code.sf.net/p/webissues/code/trunk/server@1568 c8fa975d-6fd4-4e76-b3b8-1a99b2ee3549

--- client/tools/about.html.php
+++ client/tools/about.html.php
@@ -26,7 +26,7 @@
 </fieldset>
 
 <fieldset class="form-fieldset">
-<legend><?php echo $this->imageAndText( '/common/images/web-16.png', $this->tr( 'Web Page' ) ) ?></legend>
+<legend><?php echo $this->imageAndText( '/common/images/web-16.png', $this->tr( 'Website' ) ) ?></legend>
 
 <p><?php echo $this->tr( 'Visit %1 for more information about WebIssues.', null, '<a href="http://webissues.mimec.org">webissues.mimec.org</a>' ) ?></p>
 
@@ -42,6 +42,57 @@
 </div>
 
 </fieldset>
+
+<?php if ( $checkLastVersion ): ?>
+
+<fieldset class="form-fieldset">
+<legend><?php echo $this->imageAndText( '/common/images/status-info-16.png', $this->tr( 'Latest Version' ) ) ?></legend>
+
+<?php
+switch ( $checkState ):
+case 'current':
+?>
+
+<p><?php echo $this->tr( 'Your version of WebIssues is up to date.' ) ?></p>
+
+<?php
+break;
+case 'available':
+?>
+
+<p><?php echo $this->tr( 'The latest version of WebIssues is %1.', null, $updateVersion ) ?></p>
+
+<div class="form-submit">
+<?php $form->renderSubmit( $this->tr( 'Release Notes' ), 'notes' ) ?>
+<?php $form->renderSubmit( $this->tr( 'Download' ), 'download' ) ?>
+</div>
+
+<?php
+break;
+case 'error':
+?>
+
+<p class="error"><?php echo $this->tr( 'Checking for latest version failed.' ) ?></p>
+
+<?php
+break;
+default:
+?>
+
+<p><?php echo $this->tr( 'Check for latest version of WebIssues.' ) ?></p>
+
+<div class="form-submit">
+<?php $form->renderSubmit( $this->tr( 'Check' ), 'lastVersion' ) ?>
+</div>
+
+<?php
+break;
+endswitch
+?>
+
+</fieldset>
+
+<?php endif ?>
 
 <div class="form-submit">
 <?php $form->renderSubmit( $this->tr( 'OK' ), 'ok' ) ?>
