aab87b3c04db863d9f035eeca23751252e5e571a
mimecorg <mimec@mimec.org>
2013-12-05 17:51:28 +0000

    prepared for release 1.1.1
    git-svn-id: svn+ssh://svn.code.sf.net/p/webissues/code/trunk/server@2112 c8fa975d-6fd4-4e76-b3b8-1a99b2ee3549

--- system/bootstrap.inc.php
+++ system/bootstrap.inc.php
@@ -33,7 +33,7 @@ if ( !function_exists( 'mb_strlen' ) )
 /**
 * Current version of WebIssues Server.
 */
-define( 'WI_VERSION', '1.1' );
+define( 'WI_VERSION', '1.1.1' );
 
 /**
 * Current version of the WebIssues protocol.
